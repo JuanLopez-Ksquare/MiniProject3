556d42e5424e6ee410449613f7ea150aefc3dccb
Juan Carlos <102171185+JuanLopez-Ksquare@users.noreply.github.com>
2022-05-02 15:55:00 -0500

    Completed
    Added clear method

--- server.ts
+++ server.ts
@@ -8,30 +8,38 @@ type BalanceHistory = {
 
 //Database variables
 let balanceHistory : BalanceHistory[] = [];
-let currentBalance: number = 220;
-
-
-
+let currentBalance: number = 0;
 
 //Server variables
 const app: Express = express();
 const port = 3000;
 
+app.use(express.json());
+
 //Endpoints
+
+//GetBalance
 app.get("/v1/balance", (req, res) =>{
     res.json(currentBalance);
 })
+//Add transaction
 app.post("/v1/transaction", (req, res) =>{
 
     try{
         currentBalance += req.body.incomingAmount;
         const history : BalanceHistory = {amount: req.body.incomingAmount, motive: req.body.motive}
         balanceHistory.push(history);
-        console.log(balanceHistory);
+        //console.log(balanceHistory);
         res.sendStatus(200);
     }catch{
         res.sendStatus(500);
     }
+})
+//Clear all transactions
+app.post("/v1/clear", (req, res) =>{
+    currentBalance = 0;
+    balanceHistory = [];
+    res.sendStatus(200);
 })
 
 
